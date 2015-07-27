e800dc33febf894ab764708a1dbf2bac487a61a3
Ben McLean <ben.mclean@cgs.act.edu.au>
2015-07-27 10:11:51 +1000

    Bug fix comment added

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         println("Hello World")
+        
+        //Fix thing
     }
 
     override func didReceiveMemoryWarning() {
