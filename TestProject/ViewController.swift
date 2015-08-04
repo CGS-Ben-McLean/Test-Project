c88cd335d7fb2795b8b7da4e7876ed1c575cacb7
Ben McLean <mcleanb@arctro.com>
2015-08-04 12:37:30 +1000

    Renamed Function

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
         println("This is an amazing feature")
     }
     
-    func awesomeFeatureQZ(){
+    func awesomeFeatureU(){
 
         println("This is another cool feature")
         
